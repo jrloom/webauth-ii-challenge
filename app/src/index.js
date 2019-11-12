376a2313996a118f3bc778c46ee4691f4b74bc86
Jason Loomis <jason@jrloomis.com>
2019-11-12 18:42:29 -0500

    wrap app in cookie provider

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { CookieProvider } from "react-cookie";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <CookieProvider>
+    <Router>
+      <App />
+    </Router>
+  </CookieProvider>,
   document.getElementById("root")
 );
