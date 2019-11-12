9e900f1ebcf23cdff55b3b89c6bd141a12a18760
Jason Loomis <jason@jrloomis.com>
2019-11-12 17:57:06 -0500

    user migration created, database setup

--- data/migrations/20191112154809_users.js
+++ data/migrations/20191112154809_users.js
@@ -1,8 +1,15 @@
+module.exports.up = function(knex) {
+  return knex.schema.createTable("users", users => {
+    users.increments();
 
-exports.up = function(knex) {
-  
+    users
+      .string("username", 128)
+      .notNullable()
+      .unique();
+    users.string("password", 128).notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+module.exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("users");
 };
