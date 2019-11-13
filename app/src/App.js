f63d3b6bcd15c8ab0a1b0155f6afce2245e9f2f2
Jason Loomis <jason@jrloomis.com>
2019-11-12 19:02:16 -0500

    import forms, router

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { withCookies } from "react-cookie";
+
+import Login from "./Forms/Login";
+import Register from "./Forms/Register";
+import Router from "./Utils/Router";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <div>div!</div>
+      <Register />
+      <Login />
+      <Router />
     </div>
   );
 }
 
-export default App;
+export default withCookies(App);
